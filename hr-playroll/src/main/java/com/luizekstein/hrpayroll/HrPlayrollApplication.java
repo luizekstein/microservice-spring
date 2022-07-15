fa8b81c13f45408657c0f68cca4fb412b6fc8c51
Luiz Ekstein <luiz_fekstein@hotmail.com>
2022-07-14 21:14:05 -0300

    <chore>[microservice]: <project hr-payroll created>

---
+++ hr-playroll/src/main/java/com/luizekstein/hrpayroll/HrPlayrollApplication.java
@@ -0,0 +1,13 @@
+package com.luizekstein.hrpayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrPlayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrPlayrollApplication.class, args);
+	}
+
+}
---
+++ hr-playroll/src/test/java/com/luizekstein/hrpayroll/HrPlayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizekstein.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPlayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
